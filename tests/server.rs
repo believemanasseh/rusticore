71feb53392fc1a924703e1ed2427aefb91b97bb3
Manasseh <believemanasseh@gmail.com>
2025-07-20 03:35:26 +0100

    test: modify test function

--- tests/server.rs
+++ tests/server.rs
@@ -4,6 +4,8 @@ use rusticore::ServerState;
 #[test]
 fn test_server() {
     let server = run_server(String::from("localhost"), 8080, false, None);
-    let is_running = server.check_state(ServerState::Running).0;
+    assert!(server.is_ok(), "Server should start successfully");
+
+    let is_running = server.ok().unwrap().check_state(ServerState::Running).0;
     assert_eq!(is_running, true, "Server should return true on success");
 }
